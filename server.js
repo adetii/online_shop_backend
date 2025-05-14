858c3377e51f312636d38621e8c19898ef58c403
Adeti Francis Elorm <150345054+adetii@users.noreply.github.com>
2025-05-14 07:27:35 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
